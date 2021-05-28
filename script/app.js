f2464080849ffa7fc7798746bab08ba545678953
Arjun Doel <arjundoel@gmail.com>
2021-05-28 13:05:44 +0100

    basic grid complete

--- script/app.js
+++ script/app.js
@@ -1,7 +1,55 @@
 function init() {
 
+//! DOM Elements
+const grid = document.querySelector('.grid')
 
-console.log('Hello');
+// ! Grid Properties
+const width = 20
+const cellCount = width * width
+const cells = []
+
+// ! Pac-Man Properties
+const startPosition = 0
+let currentPositon = 0
+const pacClass = 'yellow'
+
+// ! Spawn Pac
+function addPac(position) {
+  cells[position].classList.add(pacClass)
+}
+
+// ! Log grid to DOM
+function createGrid(startPosition){
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement('div')
+    cell.innerText = i
+    grid.appendChild(cell)
+    cells.push(cell)
+  }
+
+  //? Spawn Pacman @ start
+  addPac(startPosition)
+}
+
+// ! Move PacMan
+function
+
+
+
+document.addEventListener('keydown', movement)
+createGrid(startPosition)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
