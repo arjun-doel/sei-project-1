83c297e74d02dfeb9ddbd6622289c7a09f407c2a
Arjun Doel <arjundoel@gmail.com>
2021-05-28 14:50:37 +0100

    auto movement start

--- script/app.js
+++ script/app.js
@@ -1,59 +1,91 @@
 function init() {
 
-  //! DOM Elements
+  //* DOM Elements
   const grid = document.querySelector('.grid')
 
-  //! Grid Properties
+  //* Grid Properties
   const width = 20
   const cellCount = width * width
   const cells = []
 
-  //! Pac-Man Properties
+  //* Pac-Man Properties
   const startPosition = 0
   let currentPositon = 0
   const pacClass = 'yellow'
+  const blockClass = 'blue'
 
-  //! Movement Control Properties
+  //* Movement Control Properties
   const left = 37
   const right = 39
   const up = 38
   const down = 40
 
 
-  //! Log grid to DOM
+  //* Log grid to DOM
   function createGrid(startPosition) {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       cell.classList.add('cell')
       cell.innerText = i
-      cell[i].classList.add('blue')
       grid.appendChild(cell)
       cells.push(cell)
     }
     //? Spawn Pacman @ start
     addPac(startPosition)
+
+    //? Spawn Block
+    addBlock(3)
   }
 
-  console.log(cells);
 
-  //! Spawn Pac
+  //* Spawn Blocks
+  function addBlock (position) {
+    cells[position].classList.add(blockClass)
+  }
+
+  //* Spawn Pac
   function addPac(position) {
     cells[position].classList.add(pacClass)
   }
 
-  // ! Remove Pac position
+  //* Remove Pac position
   function removePac(position) {
     cells[position].classList.remove(pacClass)
   }
 
-  //! Move PacMan
+  //! PAC MAN Movement
+
+  //* Move pac right
+  function pacRight(){
+    removePac(currentPositon)
+    currentPositon++
+    addPac(currentPositon)
+  }
+
+  //* Move pac right
+  function pacRight(){
+    removePac(currentPositon)
+    currentPositon++
+    addPac(currentPositon)
+  }
+  
+  function pacLeft(){
+    removePac(currentPositon)
+    currentPositon--
+    addPac(currentPositon)
+  }
+
+  //* Move PacMan
   function movement(e) {
     const keyPress = e.keyCode
     removePac(currentPositon)
     if (keyPress === right && currentPositon % width !== width - 1) {
-      currentPositon++
+      // currentPositon++
+      const right = setInterval(pacRight, 200)
     } else if (keyPress === left && currentPositon % width !== 0){
-      currentPositon--
+      // currentPositon--
+      clearInterval(right)
+      // setInterval(pacLeft, 200)
     } else if (keyPress === up && currentPositon >= width){
       currentPositon -= width
     } else if (keyPress === down && currentPositon + width <= width * width - 1){
@@ -62,9 +94,26 @@ function init() {
       console.log('Wrong Key!');
     }
 
-
+    console.log(currentPositon);
     addPac(currentPositon)
   }
+
+  function auto() {
+    // grid.forEach(sqr => sqr.classList.remove(catClass))
+    addPac(currentPositon)
+    // const gridDiv = grid[Math.floor(Math.random() * grid.length)]
+    // gridDiv.classList.add('mole')
+    currentPositon ++
+    removePac(currentPositon)
+
+  }
+
+
+  function move() {
+    timeId = setInterval(auto, 200)
+  }
+
+  // move()
 
 
 
