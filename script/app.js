7193620d18a6863bb8e6efb0dc43ec67d5ea74f2
Arjun Doel <arjundoel@gmail.com>
2021-05-28 14:15:01 +0100

    so far d1

--- script/app.js
+++ script/app.js
@@ -1,43 +1,75 @@
 function init() {
 
-//! DOM Elements
-const grid = document.querySelector('.grid')
+  //! DOM Elements
+  const grid = document.querySelector('.grid')
 
-// ! Grid Properties
-const width = 20
-const cellCount = width * width
-const cells = []
+  //! Grid Properties
+  const width = 20
+  const cellCount = width * width
+  const cells = []
 
-// ! Pac-Man Properties
-const startPosition = 0
-let currentPositon = 0
-const pacClass = 'yellow'
+  //! Pac-Man Properties
+  const startPosition = 0
+  let currentPositon = 0
+  const pacClass = 'yellow'
 
-// ! Spawn Pac
-function addPac(position) {
-  cells[position].classList.add(pacClass)
-}
+  //! Movement Control Properties
+  const left = 37
+  const right = 39
+  const up = 38
+  const down = 40
 
-// ! Log grid to DOM
-function createGrid(startPosition){
-  for (let i = 0; i < cellCount; i++) {
-    const cell = document.createElement('div')
-    cell.innerText = i
-    grid.appendChild(cell)
-    cells.push(cell)
+
+  //! Log grid to DOM
+  function createGrid(startPosition) {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.classList.add('cell')
+      cell.innerText = i
+      cell[i].classList.add('blue')
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+    //? Spawn Pacman @ start
+    addPac(startPosition)
   }
 
-  //? Spawn Pacman @ start
-  addPac(startPosition)
-}
+  console.log(cells);
 
-// ! Move PacMan
-function
+  //! Spawn Pac
+  function addPac(position) {
+    cells[position].classList.add(pacClass)
+  }
+
+  // ! Remove Pac position
+  function removePac(position) {
+    cells[position].classList.remove(pacClass)
+  }
+
+  //! Move PacMan
+  function movement(e) {
+    const keyPress = e.keyCode
+    removePac(currentPositon)
+    if (keyPress === right && currentPositon % width !== width - 1) {
+      currentPositon++
+    } else if (keyPress === left && currentPositon % width !== 0){
+      currentPositon--
+    } else if (keyPress === up && currentPositon >= width){
+      currentPositon -= width
+    } else if (keyPress === down && currentPositon + width <= width * width - 1){
+      currentPositon += width
+    } else  {
+      console.log('Wrong Key!');
+    }
+
+
+    addPac(currentPositon)
+  }
 
 
 
-document.addEventListener('keydown', movement)
-createGrid(startPosition)
+  document.addEventListener('keydown', movement)
+  createGrid(startPosition)
 
 
 
