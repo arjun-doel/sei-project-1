5700f19cad3a3f093649d9b9f19add04ec900e36
Arjun Doel <arjundoel@gmail.com>
2021-05-31 12:06:05 +0100

    points start, come back to block

--- script/app.js
+++ script/app.js
@@ -23,18 +23,26 @@ function init() {
     // Left Edge
     0,20,40,60,80,100,120,260,240,280,300,320,340,360,380,
     // Left Top Spacing
-    241,242,243,244,224,204,203,202,201,200,
+    241,242,243,223,203,202,201,200,
     // Left Bottom Spacing
-    121,122,123,124,144,164,163,162,161,160,
+    121,122,123,143,163,162,161,160,
     // Right Edge
     19,39,59,79,99,119,139,179,219,259,279,299,319,339,359,379,399,
     // Right Top Spacing
-    138,137,136,135,155,175,176,177,178,
+    138,137,136,156,176,177,178,
     // Right Bottom Spacing
-    258,257,256,255,235,215,216,217,218
+    258,257,256,236,216,217,218,
+    // Bottom border
+    381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,
+    // Top Border
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
   ]
 
-  console.log(blockArray.length);
+  //* Point Properties
+  let score;
+  const pointClass = 'points'
+  const pointsArray = [107,108,109]
+
 
   //* Movement Control Properties
   const left = 37
@@ -57,13 +65,22 @@ function init() {
 
     //? Spawn Blocks
     addBlock(blockArray)
+
+    //? Spawn Points
+    spawnPoint(pointsArray)
   }
+
 
 
   //* Spawn Blocks
   function addBlock(index) {
     // cells[position].classList.add(blockClass)
     index.forEach(ite => cells[ite].classList.add(blockClass))
+  }
+
+  function spawnPoint(index) {
+    // cells[position].classList.add(blockClass)
+    index.forEach(ite => cells[ite].classList.add(pointClass))
   }
 
   //* Spawn Pac
@@ -133,7 +150,7 @@ function init() {
   //* Move PacMan
   function movement(e) {
     const keyPress = e.keyCode
-    
+
     removePac(currentPositon)
     if (keyPress === right && currentPositon % width !== width - 1) {
       pacRight()
@@ -146,12 +163,17 @@ function init() {
     } else {
       console.log('Wrong Key!');
     }
-
-    console.log(currentPositon);
     addPac(currentPositon)
+    addPoints(currentPositon)
   }
 
+  //* Add points
+  function addPoints(pos) {
 
+    if (pos.classList.contains(pointClass)){
+      console.log('SCORE 1');
+    }
+  }
 
 
 
